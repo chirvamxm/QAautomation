556d227182f7e0e682a237f95912a58efe4de51b
Max <chirvamaxym@gmail.com>
2022-06-20 17:39:16 +0300

    Add TetsSorting Class

---
+++ src/test/java/Tests/ZakazUA/TestHeader.java
@@ -0,0 +1,25 @@
+package Tests.ZakazUA;
+
+import Tests.PageObject.ZakazUAHomePageWebElements;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestHeader extends TestInit{
+    @Test
+    public void testHeader(){
+        ZakazUAHomePageWebElements zakazUAHomePageWebElements=new ZakazUAHomePageWebElements(driver);
+
+        goToZakazUAHomePage();
+        fullScreen();
+        Assert.assertTrue(zakazUAHomePageWebElements.getZakazLogo().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.selectLanguageBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.partnersBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.vacanciesBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.helpBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.contactsBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.chooseStoreBtn().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.donateZSUBtn().isDisplayed());
+
+
+    }
+}
