8d3bbad06be1b1b4a88a7116afdb82bcaa7d5692
Max <chirvamaxym@gmail.com>
2022-06-28 13:33:08 +0300

    AddTestAppSection

---
+++ src/test/java/Tests/ZakazUA/TestAppSection.java
@@ -0,0 +1,20 @@
+package Tests.ZakazUA;
+
+import Tests.PageObject.ZakazUAHomePageWebElements;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestAppSection extends TestInit{
+
+    @Test
+    public void testAppSection(){
+        ZakazUAHomePageWebElements zakazUAHomePageWebElements=new ZakazUAHomePageWebElements(driver);
+
+        goToZakazUAHomePage();
+        fullScreen();
+        Assert.assertTrue(zakazUAHomePageWebElements.googlePlayApp().isDisplayed());
+        Assert.assertTrue(zakazUAHomePageWebElements.appStoreApp().isDisplayed());
+
+
+    }
+}
